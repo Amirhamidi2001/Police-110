d0de178d9a494ecbbf17003468990f3d38a3b9e2
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2024-02-15 19:50:50 +0330

    Add rest_framework

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('police/', include('police.urls'))
+    path('police/', include('police.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
